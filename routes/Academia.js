f73057be1abfc84ecb17653ca9f4bfdf022cf700
CalEric <eca.2889@gmail.com>
2020-06-14 01:03:05 -0500

    Agrego archivo Academia.js a routes

---
+++ routes/Academia.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+var mongoose = require('mongoose');
+var Academia = require('../models/Academia');
+
+//AQUI VAN LOS METODOS POST,GET,PATCH, ETC
+
+module.exports = router;
