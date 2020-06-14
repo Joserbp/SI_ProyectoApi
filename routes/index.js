88464a52ffd782a9a0d465c9579fda48f4c632bf
Joserbp <josera.jack@gmail.com>
2020-06-13 22:26:26 -0500

    index.js se agrega render para el formulario y se agregaron rutas al app.js

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/api/autos/post', function(req, res, next) {
+  res.render('Formulario');
+});
+
 module.exports = router;
