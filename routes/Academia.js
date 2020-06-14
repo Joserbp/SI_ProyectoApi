278030f8d451fe8732d89a909b376579d9648b84
Joserbp <josera.jack@gmail.com>
2020-06-14 16:15:21 -0500

    Metodo post funcionando con el formulario

--- routes/Academia.js
+++ routes/Academia.js
@@ -5,5 +5,29 @@ var mongoose = require('mongoose');
 var Academia = require('../models/Academia');
 
 //AQUI VAN LOS METODOS POST,GET,PATCH, ETC
+router.post('/', function(req, res, next) {
+
+  var academia=  Academia({
+    Id: req.body.Id,
+    Nombre: req.body.Nombre,
+    Director: req.body.Director,
+	Ritmo_Principal: req.body.Ritmo_Principal,
+	Ritmo_Secundario: req.body.Ritmo_Secundario,
+	Direccion: req.body.Direccion,
+	num_Profesores: req.body.num_Profesores,
+	num_Alumnos: req.body.num_Alumnos,
+  });
+
+  academia.save(function(err,data){
+    if (err) {
+      res.status(404).json({mensaje: 'Error al registrar'});
+    }else {
+      res.status(201).json({mensaje: 'Registro correcto'})
+    }
+  });
+
+});
+
+
 
 module.exports = router;
